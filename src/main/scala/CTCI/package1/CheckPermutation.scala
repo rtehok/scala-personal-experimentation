f06dfaff6e68a05f087f1212c52994aea166ad92
Raymond TeHok <raymond.tehok@amadeus.com>
2018-07-31 09:41:30 +0200

    1.2 Check Permutations

--- src/main/scala/CTCI/package1/CheckPermutation.scala
+++ src/main/scala/CTCI/package1/CheckPermutation.scala
@@ -1,5 +1,34 @@
 package CTCI.package1
 
-class CheckPermutation {
+object CheckPermutation {
+
+  def hasSameSize(s1: String, s2: String): Boolean = s1.length == s2.length
+
+  // Using sort O(n log(n))
+  def verify(s1: String, s2: String): Boolean = {
+    val s1a = s1.toLowerCase.sorted.trim
+    val s2a = s2.toLowerCase.sorted.trim
+    if (!hasSameSize(s1a, s2a)) return false
+
+    s1a.equals(s2a)
+  }
+
+  // Whitespace and case sensitive
+  def verify2(s1: String, s2: String): Boolean = {
+    if (!hasSameSize(s1, s2)) return false
+
+    val s_array = Array.fill(128){0}
+
+    s1.toList.foreach { c =>
+      s_array(c) = s_array(c) + 1
+    }
+
+    s2.toList.foreach{ c =>
+      s_array(c) = s_array(c) - 1
+      if (s_array(c) < 0) return false
+    }
+
+    true
+  }
 
 }
--- src/test/scala/CTCI/package1/CheckPermutationTest.scala
+++ src/test/scala/CTCI/package1/CheckPermutationTest.scala
@@ -1,5 +1,30 @@
 package CTCI.package1
 
-class CheckPermutationTest {
+import org.scalatest.{FunSpec, Matchers}
+
+class CheckPermutationTest extends FunSpec with Matchers {
+
+  describe("CheckPermutation") {
+    it("verifies") {
+      CheckPermutation.verify("","a") shouldBe false
+      CheckPermutation.verify("  ","a") shouldBe false
+      CheckPermutation.verify("  "," ") shouldBe true
+      CheckPermutation.verify("aa","a") shouldBe false
+      CheckPermutation.verify("aa","a a") shouldBe true
+      CheckPermutation.verify("ab","ba") shouldBe true
+      CheckPermutation.verify("Dog","God") shouldBe true
+    }
+
+    it("verifies 2") {
+      CheckPermutation.verify2("","a") shouldBe false
+      CheckPermutation.verify2("  ","a") shouldBe false
+      CheckPermutation.verify2("  "," ") shouldBe false
+      CheckPermutation.verify2("aa","a") shouldBe false
+      CheckPermutation.verify2("aa","a a") shouldBe false
+      CheckPermutation.verify2("ab","ba") shouldBe true
+      CheckPermutation.verify2("Dog","God") shouldBe false
+      CheckPermutation.verify2("dog","god") shouldBe true
+    }
+  }
 
 }

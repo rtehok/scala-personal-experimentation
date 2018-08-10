53bb55b1d1f3760963b955dafb7c8af92dc497ba
Raymond TeHok <raymond.tehok@amadeus.com>
2018-08-10 14:28:29 +0200

    Add tests to 1.3 URLify

--- src/test/scala/CTCI/package1/URLifyTest.scala
+++ src/test/scala/CTCI/package1/URLifyTest.scala
@@ -8,6 +8,7 @@ class URLifyTest extends FunSpec with Matchers {
     it("should work") {
       URLify.verify("Mr John Smith    ", 13) shouldEqual "Mr%20John%20Smith"
       URLify.verify("Mr  John Smith      ", 14) shouldEqual "Mr%20%20John%20Smith"
+      URLify.verify("Mr John Smith    ", 12) shouldEqual "Mr%20John%20Smit "
     }
   }
 

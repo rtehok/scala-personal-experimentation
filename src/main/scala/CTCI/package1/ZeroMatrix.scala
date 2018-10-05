364673a25a1215082df705df8536d99ab6774b41
Raymond TeHok <raymond.tehok@amadeus.com>
2018-10-05 09:52:08 +0200

    Reorganize code

--- src/main/scala/CTCI/package1/ZeroMatrix.scala
+++ src/main/scala/CTCI/package1/ZeroMatrix.scala
@@ -7,39 +7,33 @@ object ZeroMatrix {
     val row = mutable.ArrayBuffer.fill(matrix.size)(false)
     val column = mutable.ArrayBuffer.fill(matrix(0).size)(false)
 
-//    matrix.zipWithIndex.foreach { case (matrix_row, i) =>
-//      matrix_row.zipWithIndex.foreach {  case (v, j) =>
-//        if (v == 0) {
-//          row(i) = true
-//          column(j) = true
-//        }
-//      }
-//    }
+    def nullifyRow(m: mutable.ArrayBuffer[mutable.ArrayBuffer[Int]], row: Int): Unit = {
+      m(row) = mutable.ArrayBuffer.fill(column.size)(0)
+    }
 
+    def nullifyColumn(m: mutable.ArrayBuffer[mutable.ArrayBuffer[Int]], col: Int): Unit = {
+      m.foreach {
+        _.update(col, 0)
+      }
+    }
+
+    //First step need to search for indices
     for {
       i <- matrix.indices
       j <- matrix(i).indices if matrix(i)(j) == 0
     } (row(i) = true, column(j) = true)
 
-  def nullifyRow(m: mutable.ArrayBuffer[mutable.ArrayBuffer[Int]], row: Int): Unit = {
-    m(row) = mutable.ArrayBuffer.fill(column.size)(0)
+    //Modify the matrix
+    for {
+      i <- matrix.indices if row(i)
+    } nullifyRow(matrix, i)
+
+    for {
+      j <- matrix(0).indices if column(j)
+    } nullifyColumn(matrix, j)
+
+    matrix
+
   }
-
-  def nullifyColumn(m: mutable.ArrayBuffer[mutable.ArrayBuffer[Int]], col: Int): Unit = {
-    m.foreach {
-      _.update(col, 0)
-    }
-  }
-
-  for {
-    i <- matrix.indices if row(i)
-  } nullifyRow(matrix, i)
-
-  for {
-    j <- matrix(0).indices if column(j)
-  } nullifyColumn(matrix, j)
-
-  matrix
-}
 
 }
